a096df04bf874cd314f3f208862ac79e64aedc3a
James Cumming <Cumming.James@gmail.com>
2019-03-29 16:56:21 -0400

    User and CitizenUser

--- SmartSaugaServer/src/main/java/ca/smartsauga/beans/CitizenUser.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/beans/CitizenUser.java
@@ -1,5 +1,31 @@
 package ca.smartsauga.beans;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class CitizenUser extends User {
+	
+	//Needs Status Enum eventually
+	private String userStatus;
+	private LocalDate joinDate;
+	private LocalDate lastLoggedDate;
+	
+	public CitizenUser(int userId, String email, String imageFileName, 
+			String userStatus, LocalDate joinDate, LocalDate lastLoggedDate) {
+		super(userId, email, imageFileName);
+		this.userStatus = userStatus;
+		this.joinDate = joinDate;
+		this.lastLoggedDate = lastLoggedDate;
+		
+	}
+	
+	
 
 }
--- SmartSaugaServer/src/main/java/ca/smartsauga/beans/User.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/beans/User.java
@@ -1,5 +1,25 @@
 package ca.smartsauga.beans;
+import java.time.LocalDate;
 
-public abstract class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int userId;
+	@Column(nullable = false, length = 45)
+	private String email;
+	private String imageFileName;
+	
 
 }
