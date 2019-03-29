bf2a9caef82c99d7935969b7b23a2401dadb60f0
James Cumming <Cumming.James@gmail.com>
2019-03-29 14:41:20 -0400

    Master Branch Creation

---
+++ SmartSaugaServer/src/main/java/ca/smartsauga/controllers/SmartSaugaServerApplication.java
@@ -0,0 +1,13 @@
+package ca.smartsauga.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSaugaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSaugaServerApplication.class, args);
+	}
+
+}
---
+++ SmartSaugaServer/src/test/java/ca/smartsauga/controllers/SmartSaugaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.smartsauga.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartSaugaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
