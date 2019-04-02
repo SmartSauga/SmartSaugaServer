a57378292bef79144c2f954e3afbf305b0de6ec2
Manpreet Kaur <kaur2615@sheridancollege.ca>
2019-04-02 14:27:03 -0400

    Locations parent class

---
+++ SmartSaugaServer/src/main/java/ca/smartsauga/beans/Locations.java
@@ -0,0 +1,12 @@
+package ca.smartsauga.beans;
+
+public class Locations {
+	
+	private int locationId;
+	private String name;
+	private String address;
+	private String[] messageStrings;
+	private int ratings;
+	
+	
+}
