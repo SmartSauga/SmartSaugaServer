cd1f1359568b81309d80728534e4e267d5d65b41
Manpreet Kaur <kaur2615@sheridancollege.ca>
2019-04-02 15:08:53 -0400

    Locations class - Finished

--- SmartSaugaServer/src/main/java/ca/smartsauga/beans/Locations.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/beans/Locations.java
@@ -1,12 +1,41 @@
 package ca.smartsauga.beans;
+import lombok.*;
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
+@Entity
 public class Locations {
-	
+	@Id @GeneratedValue
 	private int locationId;
+	
+	@Column(name="location_name", nullable=false, length=100)
 	private String name;
+	
+	@Column(name="location_address", nullable=false, length=150)
 	private String address;
+	
+	@Column(name="location_address",nullable=true,length=150)
 	private String[] messageStrings;
+	
 	private int ratings;
 	
+	@Column(nullable=true)
+	private String image;
 	
+	private String category;
+	
+	private int avgWifiRating;
+	
+	public Locations(String name, String address, String[] messageStrings, int ratings, String image, String category, int avgWifiRating ) {
+		this.name = name;
+		this.address = address;
+		this.messageStrings = messageStrings;
+		this.ratings = ratings;
+		this.image = image;
+		this.category = category;
+		this.avgWifiRating = avgWifiRating;
+	}
+		
 }
