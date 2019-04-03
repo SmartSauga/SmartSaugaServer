0c68e17afc55b94d6dec54424acbc187075beeca
James Cumming <Cumming.James@gmail.com>
2019-04-03 10:53:10 -0400

    Committed

--- SmartSaugaServer/src/main/java/ca/smartsauga/controllers/LoginController.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/controllers/LoginController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ca.smartsauga.controllers;
 
 import java.time.LocalDate;
@@ -59,47 +58,4 @@ public class LoginController {
 	
 
 }
-=======
-package ca.smartsauga.controllers;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import ca.smartsauga.beans.CitizenUser;
-import ca.smartsauga.beans.Locations;
-import ca.smartsauga.dao.Dao;
-
-@RestController
-public class LoginController {
-	Dao dao = new Dao();
-	
-	@CrossOrigin
-	@RequestMapping(value = "/validateUser/{email}/{password}", method = RequestMethod.POST)
-	public CitizenUser validateStudent(@PathVariable String email, @PathVariable String password) {
-		
-		//TODO: We want to check the password passed to the DAO that gets the password without instantiating a variable
-		//TODO: dao.validateUser will return a decrypted Password
-		//If Password is correct, return a CitizenUser else return null, handle at front end
-		if(password.contentEquals(dao.validateUser(email))) {
-			return dao.getValidatedUser(email);
-		} else {
-			return null;
-		}
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/GetLocations", method = RequestMethod.GET)
-	public List<Locations> getLocations() {
-		
-		//TODO: DAO Get All Locations needs to be in DAO class
-		return dao.getAllLocations();
-		
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/SmartSauga/SmartSaugaServer.git
