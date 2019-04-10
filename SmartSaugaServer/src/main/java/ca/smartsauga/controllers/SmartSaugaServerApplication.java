3876c8fd4a7c8b168e6271ac7eefd0e1adf20d55
Manpreet Kaur <kaur2615@sheridancollege.ca>
2019-04-10 10:54:37 -0400

    changes from the server

--- SmartSaugaServer/src/main/java/ca/smartsauga/controllers/SmartSaugaServerApplication.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/controllers/SmartSaugaServerApplication.java
@@ -2,9 +2,10 @@ package ca.smartsauga.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartSaugaServerApplication {
+public class SmartSaugaServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartSaugaServerApplication.class, args);
