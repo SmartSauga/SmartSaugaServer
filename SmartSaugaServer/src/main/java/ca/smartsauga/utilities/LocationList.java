d99b2fc2d40ee346112dda0a1224a1edd398e7a1
Manpreet Kaur <kaur2615@sheridancollege.ca>
2019-04-16 21:36:25 -0400

    locations latitude and longitude

--- SmartSaugaServer/src/main/java/ca/smartsauga/utilities/LocationList.java
+++ SmartSaugaServer/src/main/java/ca/smartsauga/utilities/LocationList.java
@@ -10,17 +10,17 @@ public class LocationList {
 	List<Locations> locationList = new ArrayList<Locations>();
 	
 	public List<Locations> locations() {
-		
-		locationList.add(new Locations("Lorne Park Library",43.53, 79.62));
-        locationList.add(new Locations("Benares Estate Visitor Centre",43.5272,79.6373));
-        locationList.add(new Locations("Bradley Museum-Heritage",43.5121,79.6114));
-        locationList.add(new Locations("Braeben Golf Course Academy",43.6020,79.6952));
-        locationList.add(new Locations("Burnhamthorpe Community Centre & Arena",43.6227,79.5987));
-        locationList.add(new Locations("Burnhamthorpe Library & Maja Prentice Theatre",43.6210,79.6030));
-        locationList.add(new Locations("Cawthra Community Centre & Arena",43.5781,79.5768));
-        locationList.add(new Locations("Chappell Estate House-Heritage",43.5651,79.6742));
-        locationList.add(new Locations("Churchill Meadows Branch Library & Activity Centre",43.5529, 79.7467));
-        locationList.add(new Locations("Clarkson Community Centre Library & Arena",43.5117,79.6506));
+		//name,longitude,latitude
+		locationList.add(new Locations("Lorne Park Library",-79.623770, 43.532840)); //done
+        locationList.add(new Locations("Benares Estate Visitor Centre",-79.637180,43.527530));
+        locationList.add(new Locations("Bradley Museum-Heritage",-79.611480,43.512060));
+        locationList.add(new Locations("Braeben Golf Course Academy",-79.695420,43.602560));
+        locationList.add(new Locations("Burnhamthorpe Community Centre & Arena",-79.599159,43.623268));
+        locationList.add(new Locations("Burnhamthorpe Library & Maja Prentice Theatre",-79.603075,43.621132));
+        locationList.add(new Locations("Cawthra Community Centre & Arena",-79.576730,43.578230));
+        locationList.add(new Locations("Chappell Estate House-Heritage",-79.673720,43.565630));
+        locationList.add(new Locations("Churchill Meadows Branch Library & Activity Centre",-79.730810, 43.563350));
+        locationList.add(new Locations("Clarkson Community Centre Library & Arena",-79.640877,43.519218));
 		
 		return locationList;
 	}
